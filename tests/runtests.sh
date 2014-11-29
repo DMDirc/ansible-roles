f559ae5f9ccd287d4756363eeaca6194c4443e82
Greg Holmes <git@greg.holmes.name>
2014-11-29 11:17:32 +0000

    Use docker.

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd /opt/ansible/tests
 set -e
 
 case $1 in
@@ -8,9 +8,9 @@ case $1 in
 	2) FILE=python ;;
 esac
 
-ansible-playbook -i tests/inventory tests/$FILE.yml --syntax-check
-ansible-playbook -i tests/inventory tests/$FILE.yml --connection=local --sudo
-ansible-playbook -i tests/inventory tests/$FILE.yml --connection=local --sudo \
+ansible-playbook -i inventory $FILE.yml --syntax-check
+ansible-playbook -i inventory $FILE.yml --connection=local --sudo
+ansible-playbook -i inventory $FILE.yml --connection=local --sudo \
       | grep -q 'changed=0.*failed=0' \
       && (echo 'Idempotence test: pass' && exit 0) \
       || (echo 'Idempotence test: fail' && exit 1)
